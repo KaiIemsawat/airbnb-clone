8f1999ee98a6095cde33030797d1b703a4b0df4e
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-01-27 07:51:28 -0500

    base form control

---
+++ app/components/modals/RegisterModal.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useState, useCallback } from "react";
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import {
+    FieldValue,
+    FieldValues,
+    SubmitHandler,
+    useForm,
+} from "react-hook-form";
+
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+
+const RegisterModal = () => {
+    const registerModal = useRegisterModal();
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FieldValues>({
+        defaultValues: {
+            name: "",
+            email: "",
+            password: "",
+        },
+    });
+
+    return <div>RegisterModal</div>;
+};
+export default RegisterModal;
