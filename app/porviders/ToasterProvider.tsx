98f4342a7198c3313aca9604f919bc266f753c0b
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-01-28 14:21:21 -0500

    prisma schema

--- app/porviders/ToasterProvider.tsx
+++ app/porviders/ToasterProvider.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { edgeServerAppPaths } from "next/dist/build/webpack/plugins/pages-manifest-plugin";
 import { Toaster } from "react-hot-toast";
 
 const ToasterProvider = () => {
