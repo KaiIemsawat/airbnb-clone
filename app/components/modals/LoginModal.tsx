87c61f857716e7c296c02035c80a82998dc391c9
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-02-01 09:33:42 -0500

    login/register modal toggle

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -2,7 +2,7 @@
 
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
@@ -53,6 +53,11 @@ const LoginModal = () => {
         });
     };
 
+    const toggle = useCallback(() => {
+        loginModal.onClose();
+        registerModal.onOpen();
+    }, [loginModal, registerModal]);
+
     const bodyContent = (
         <div className="flex flex-col gap-4">
             <Heading
@@ -97,12 +102,12 @@ const LoginModal = () => {
             />
             <div className="text-neutral-500 text-center mt-4 font-light">
                 <div className="flex flex-row items-center gap-2 justify-center">
-                    <div>Already have an account?</div>
+                    <div>First time using Airbnb?</div>
                     <div
                         className="text-neutral-800 cursor-pointer hover:underline"
-                        onClick={registerModal.onClose}
+                        onClick={toggle}
                     >
-                        Login
+                        Create an account
                     </div>
                 </div>
             </div>
--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -7,6 +7,7 @@ import { FcGoogle } from "react-icons/fc";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 import useRegisterModal from "@/app/hooks/useRegisterModal";
+import useLoginModal from "@/app/hooks/useLoginModal";
 import Modal from "./Modal";
 import Heading from "../Heading";
 import Input from "../inputs/input";
@@ -16,6 +17,7 @@ import { signIn } from "next-auth/react";
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
+    const loginModal = useLoginModal();
 
     const [isLoading, setIsLoading] = useState(false);
 
@@ -46,6 +48,11 @@ const RegisterModal = () => {
                 setIsLoading(false);
             });
     };
+
+    const toggle = useCallback(() => {
+        registerModal.onClose();
+        loginModal.onOpen();
+    }, [loginModal, registerModal]);
 
     const bodyContent = (
         <div className="flex flex-col gap-4">
@@ -102,7 +109,7 @@ const RegisterModal = () => {
                     <div>Already have an account?</div>
                     <div
                         className="text-neutral-800 cursor-pointer hover:underline"
-                        onClick={registerModal.onClose}
+                        onClick={toggle}
                     >
                         Login
                     </div>
