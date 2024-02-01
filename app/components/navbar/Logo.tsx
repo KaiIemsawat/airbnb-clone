3759a751b338b181ae06844b4e0c2d7cd6c1c384
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-02-01 09:23:03 -0500

    logo to go back to home

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 const Logo = () => {
+    const router = useRouter();
     return (
         <Image
             className="hidden md:block cursor-pointer"
@@ -11,6 +12,9 @@ const Logo = () => {
             height="100"
             width="100"
             src="/images/logo.png"
+            onClick={() => {
+                router.push("/");
+            }}
         />
     );
 };
