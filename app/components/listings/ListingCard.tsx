e9d094c4fa45b782997b89dc1ebabdeb8c3b40ef
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-02-04 18:49:36 -0500

    ListingCard (category and price)

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -9,6 +9,7 @@ import { Listing, Reservation } from "@prisma/client";
 import useCountries from "@/app/hooks/useCountry";
 import Image from "next/image";
 import HeartButton from "../HeartButton";
+import Button from "../Button";
 
 interface ListingCardProps {
     data: Listing;
@@ -99,6 +100,21 @@ const ListingCard: React.FC<ListingCardProps> = ({
                 <div className="font-semibold text-lg">
                     {location?.region}, {location?.label}
                 </div>
+                <div className="font-light text-neutral-500">
+                    {reservationDate || data.category}
+                </div>
+                <div className="flex flex-row items-center gap-1">
+                    <div className="font-semibold">$ {price}</div>
+                    {!reservation && <div className="font-light">night</div>}
+                </div>
+                {onAction && actionLabel && (
+                    <Button
+                        disable={disabled}
+                        small
+                        label={actionLabel}
+                        onClick={handleCancel}
+                    />
+                )}
             </div>
         </div>
     );
